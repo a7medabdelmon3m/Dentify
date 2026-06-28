071b370889c6360f1ffc2609701f1eb76cf5d8dd
Ahmed Moneim <ahnedmoneimhacher26@gmail.com>
2026-06-28 18:31:14 +0300

    fixing appointments page of patient

--- src/app/[locale]/(patient)/patient/appointment/page.tsx
+++ src/app/[locale]/(patient)/patient/appointment/page.tsx
@@ -30,32 +30,36 @@ import { apiRequest } from "@/app/api/services/denti.services";
 
 export default async function PatientAppointmentPage() {
   const t = await getTranslations("patientAppointment");
-  const appoints = await apiRequest<appointmentType[]>('http://localhost:5123/api/Appointments/My/Patient');
+  const appoints = await apiRequest<appointmentType[]>(
+    "http://localhost:5123/api/Appointments/My/Patient",
+  );
 
-  console.log('appoints : ' , appoints );
-  
+  console.log("appoints : ", appoints);
 
   if (!appoints || !appoints.data || appoints.data.length === 0) {
-  return <NoAppointments />;
-}
-
+    return <NoAppointments />;
+  }
+  const currentCaseId = appoints.data?.[0]?.caseId;
   return (
     <div className="flex w-full flex-col p-6 text-center bg-bg-main">
       <div className="mx-auto w-full max-w-2xl space-y-6 text-rightAr">
-        
         {/* رأس الصفحة السيرفر */}
         <div className="flex flex-col gap-1.5 mb-8">
           <h1 className="font-heading text-3xl font-bold tracking-tight text-text-title">
             {t("pageTitle")}
           </h1>
           <p className="text-sm font-medium text-text-muted">
-            {t("pageDesc")} <span className="font-bold text-primary">#{appoints.data?.[0].caseId}</span>
+            {t("pageDesc")}{" "}
+            <span className="font-bold text-primary">
+              #{currentCaseId}
+            </span>{" "}
           </p>
         </div>
 
         {/* الكومبوننت التفاعلي */}
-        <AppointmentSelectionCard appointments={appoints.data as appointmentType[] } />
-
+        <AppointmentSelectionCard
+          appointments={appoints.data as appointmentType[]}
+        />
       </div>
     </div>
   );
